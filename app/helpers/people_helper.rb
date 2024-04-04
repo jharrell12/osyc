1d34f6910d33939b662da6ac3b6f4dc0897a0923
J Harrell <joseph.harrell@riverside.net>
2024-04-03 20:17:33 -0400

    some stuff on the view membership page

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -1,2 +1,10 @@
 module PeopleHelper
+
+  def person_actions(person)
+    [navbar_button_group do |s|
+      s << edit_button(nil, edit_person_path(person))
+      s << delete_button(nil, person_path(person), "Person #{person.id}")
+    end, 1]
+  end
+
 end
