ef7d0855f0159d0f83b75bd2f517b1dc78e1200a
Jay <jay.harrell@riverside.net>
2024-04-08 19:08:18 -0400

    create credentials file

--- app/models/concerns/start_end_dates.rb
+++ app/models/concerns/start_end_dates.rb
@@ -6,7 +6,7 @@ module StartEndDates
     attribute :start_date, default: -> { Date.today }
     validates :end_date, comparison: { greater_than_or_equal_to: :start_date }, allow_blank: true
     scope :sorted, -> { order(end_date: :asc, label: :asc, start_date: :desc) }
-    scope :current,         -> { sorted.where("(end_date is null) OR DATE(end_date) >= DATE('now')") }
+    scope :current,         -> { sorted.where("(end_date is null) OR DATE(end_date) > DATE('now')") }
   end
 
   def current? = end_date.nil? || end_date > Date.today  
