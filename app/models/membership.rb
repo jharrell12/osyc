a5f2b54c3a60181a5eef8a3d2fa180fd6787ede5
Jay Harrell <jay.harrell@riverside.net>
2024-04-10 17:02:51 -0400

    fix phones str

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -50,13 +50,7 @@ class Membership < ApplicationRecord
     #phones.current.collect{|phone| "#{phone.label}: #{phone.number_str} #{phone.person.first_name} ".to_nb}
     
     # refactored to avoid n+1 query on index page
-    #phones.select{|phone| phone.current?}.collect{|phone| "#{phone.label}: #{phone.number_str} #{phone.person.first_name} ".to_nb}
-
-    people.collect do |person|
-      phones.select{|phone| phone.current?}.collect do |phone|
-        "#{phone.label}: #{phone.number_str} #{person.first_name} ".to_nb
-      end
-    end    
+    phones.select{|phone| phone.current?}.collect{|phone| "#{phone.label}: #{phone.number_str} #{phone.person.first_name} ".to_nb} 
   end
 
 end
