13fbc3cf82ec96affa7319fbd79e3012ab3380ae
J Harrell <joseph.harrell@riverside.net>
2024-04-14 21:52:50 -0400

    some invoices updates

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -26,7 +26,7 @@ class InvoicesController < ApplicationController
 
     respond_to do |format|
       if @invoice.save
-        format.html { redirect_to invoice_url(@invoice), notice: "Invoice was successfully created." }
+        format.html { redirect_to membership_url(@invoice.membership_id), notice: "Invoice was successfully created." }
         format.json { render :show, status: :created, location: @invoice }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -39,7 +39,7 @@ class InvoicesController < ApplicationController
   def update
     respond_to do |format|
       if @invoice.update(invoice_params)
-        format.html { redirect_to invoice_url(@invoice), notice: "Invoice was successfully updated." }
+        format.html { redirect_to membership_url(@invoice.membership_id), notice: "Invoice was successfully updated." }
         format.json { render :show, status: :ok, location: @invoice }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -53,7 +53,7 @@ class InvoicesController < ApplicationController
     @invoice.destroy!
 
     respond_to do |format|
-      format.html { redirect_to invoices_url, notice: "Invoice was successfully destroyed." }
+      format.html { redirect_to membership_url(@invoice.membership_id), notice: "Invoice was successfully destroyed." }
       format.json { head :no_content }
     end
   end
--- app/helpers/invoices_helper.rb
+++ app/helpers/invoices_helper.rb
@@ -2,9 +2,30 @@ module InvoicesHelper
 
   def invoice_actions(invoice)
     [navbar_button_group do |s|
-      s << edit_button(nil, edit_invoice_path(invoice))
-      s << delete_button(nil, invoice_path(invoice), "Invoice #{invoice.id}")
+      s << edit_link(nil, edit_invoice_path(invoice))
+      s << '  '.to_nb
+      s << turbo_delete_link(nil, invoice_path(invoice), "Invoice #{invoice.id}")
     end, 1]
+  end
+
+  def invoices_caption(membership)
+    data_table_caption('Invoices', button_group_div(add_button((svg_show), invoices_path(membership_id: @membership.id))) )
+  end
+
+  def invoices_header
+    field_label_row(%w(action status posted_date due_date balace_due).map(&:titlecase))
+  end
+
+  def invoice_row(invoice)
+    data_item_row([invoice_actions(invoice), invoice.status, invoice.posted_date, invoice.due_date, invoice.balance_due])
+  end
+
+  def invoices_table(membership)
+    data_table(invoices_caption(membership), invoices_header) do |s|
+      membership.invoices.each do |invoice|
+        s << invoice_row(invoice)
+      end
+    end
   end
 
 end
