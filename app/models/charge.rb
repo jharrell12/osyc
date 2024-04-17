2f2bfc453750dcd818b05d55c2c66d05fd1d12dd
Jay <jay.harrell@riverside.net>
2024-04-16 22:59:05 -0400

    validations for charges and payments

--- app/models/charge.rb
+++ app/models/charge.rb
@@ -1,4 +1,6 @@
 class Charge < ApplicationRecord
   belongs_to :invoice
   has_one :membership, through: :invoice
+  validates_presence_of :description
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
 end
--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,4 +1,6 @@
 class Payment < ApplicationRecord
   belongs_to :invoice
   has_one :membership, through: :invoice
+  validates_presence_of :date
+  validates :amount, numericality: { greater_than: 0 }
 end
