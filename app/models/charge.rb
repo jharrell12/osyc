866392cddc77a7e00bd1d76b84155c8c4f73fbb4
J Harrell <joseph.harrell@riverside.net>
2024-04-19 16:12:15 -0400

    balance due calc

--- app/models/charge.rb
+++ app/models/charge.rb
@@ -4,6 +4,12 @@ class Charge < ApplicationRecord
   validates_presence_of :description
   validates :price, numericality: { greater_than_or_equal_to: 0 }
 
+  after_save :calculate_balance_due
+
+  def calculate_balance_due
+    self.invoice.save
+  end
+
   def charge_str
     "$" + [price, description].join(' : ')
   end
--- app/models/payment.rb
+++ app/models/payment.rb
@@ -4,6 +4,12 @@ class Payment < ApplicationRecord
   validates_presence_of :date
   validates :amount, numericality: { greater_than: 0 }
 
+  after_save :calculate_balance_due
+
+  def calculate_balance_due
+    self.invoice.save
+  end
+
   def payment_str
     "$" + [amount, check_number].join(' : ')
   end
